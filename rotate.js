bde0ebb18d2db7c2b9e64e808f3d892090bd922b
thomassth <thomas@google.com>
2019-05-28 19:26:55 +0800

    2.0

--- rotate.js
+++ rotate.js
@@ -4,17 +4,39 @@ function changeBanner() {
     [].forEach.call(list, function (v, i) {
         ///Show the current item
         list[i].hidden = i !== index;
-        if(i==index){
+        if (i == index) {
             list[i].className = "showup";
         }
-
     }
     );
     ///Rotate through the list
     index = (index + 1) % list.length;
 }
-window.onload = function () { setInterval(changeBanner, 5000) };
-
+window.onload = function () {
+    ///replace text
+    loadLines();
+    console.log="Lines loaded";
+    ///Begin display
+    var list = document.getElementById('tickerContent').children;
+    list[0].className = "showup";
+    list[0].hidden = 0;
+    console.log="First line shown";
+    ///Rest of the rotation
+    if (list.length>=2){
+        setInterval(changeBanner, 5000);
+    }else{
+        console.log="No looping effect";
+    }
+};
+loadLines = function () {
+    var linesArray = localStorage.getItem("list");
+    var readArray = JSON.parse(linesArray);
+    var area = document.getElementById("tickerContent");
+    area.innerHTML = "";
+    for (var i = 0; i < readArray.length; i++) {
+        area.innerHTML += "<p>" + readArray[i] + "</p>";
+    }
+}
 ///auto-reload
 /*window.setTimeout(function() {
     window.location.href =  window.location.href;
